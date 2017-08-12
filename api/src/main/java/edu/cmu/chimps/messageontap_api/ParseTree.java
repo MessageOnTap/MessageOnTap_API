2c462a9410c4efb75163571b89e1c3d9e294d1d8
Adam Yi <xuan@yiad.am>
2017-08-12 14:33:22 -0400

    Revert "Update ParseTree.java"
    This reverts commit 5ce34fa85214207a0d149867c0838b9ffba61818.

--- api/src/main/java/edu/cmu/chimps/messageontap_api/ParseTree.java
+++ api/src/main/java/edu/cmu/chimps/messageontap_api/ParseTree.java
@@ -391,14 +391,14 @@ public class ParseTree {
     public void moodDetection() {
         for (int i = 0; i < mNodeList.size(); i++) {
             Node node = mNodeList.valueAt(i);
-            if (!node.getType().isEmpty()){
+            if (!node.getType.isEmpty()){
                 if (TextUtils.equals(node.getType(), DEP_PUNCTUATION)
                         && TextUtils.equals(node.getWord(), "?")) {
                     mood = Mood.INTERROGATIVE;
                     return;
                 }
             }
-            if (!node.getType().isEmpty()){
+            if (!node.getType.isEmpty()){
                 if (TextUtils.equals(getRoot().getType(), POS_VERB)) {
                     for (int childId : getRoot().getChildrenIds()) {
                         if (TextUtils.equals(getNodeById(childId).getType(), DEP_NOUN_SUBJECT)) {
